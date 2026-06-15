789b8a4755f3bd06ac0b6dd49c2bdf59ebc8d3b4
niccup <niccupcosplay@gmail.com>
2026-06-15 17:17:00 +0200

    mniej

--- Assets/move.cs
+++ Assets/move.cs
@@ -7,6 +7,14 @@ public class Move : MonoBehaviour
     public float speed = 7f;
     public Rigidbody rb;
 
+    [Header("Jumping Settings")]
+    public float jumpForce = 8f;         // Adjust this value in the inspector to change the jump height
+    public Transform groundCheck;        // An empty GameObject placed at the player's feet
+    public LayerMask groundLayer;        // Set this to your Ground layer in the inspector
+    private bool isGrounded;
+    private bool jumpRequest;
+    public float fallMultiplier = 2.5f;
+
     [Space(12)]
     public Animator anim;
 
@@ -26,11 +34,34 @@ public class Move : MonoBehaviour
         {
             Flip();
         }
+
+        // 1. Check if the player is touching the ground
+        isGrounded = Physics.CheckSphere(groundCheck.position, 0.2f, groundLayer);
+        anim.SetBool("isGrounded", isGrounded);
+
+        // 2. Catch the jump input (prevents missed inputs in FixedUpdate)
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            jumpRequest = true;
+
+            anim.SetTrigger("Jump");
+        }
     }
 
     void FixedUpdate()
     {
         rb.linearVelocity = new Vector3(main1 * speed, rb.linearVelocity.y, rb.linearVelocity.z);
+
+        if (jumpRequest)
+        {
+            rb.linearVelocity = new Vector3(rb.linearVelocity.x, jumpForce, rb.linearVelocity.z);
+            jumpRequest = false;
+        }
+
+        if (rb.linearVelocity.y < 3f && !isGrounded)
+        {
+            rb.linearVelocity += Vector3.up * Physics.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+        }
     }
 
     void Flip()
