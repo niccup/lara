7b109e00ec9a79f4b90a120a004fb92594a36460
niccup <niccupcosplay@gmail.com>
2026-06-15 15:58:14 +0200

    :)

--- Assets/move.cs
+++ Assets/move.cs
@@ -1,44 +1,42 @@
 using UnityEngine;
 
-public class move : MonoBehaviour
+public class Move : MonoBehaviour
 {
-
     public float main1;
-
     public bool facingRight = true;
-
-    public float speed = 5f;
-
+    public float speed = 7f;
     public Rigidbody rb;
+
+    [Space(12)]
+    public Animator anim;
 
     void Update()
     {
         main1 = Input.GetAxis("Horizontal");
 
-        if (main1 < 0 && facingRight)
-        {
-            flip();
-        }
-        else if (main1 > 0 && !facingRight)
-        {
-            flip();
-        }
+        float flipF = facingRight ? 1 : -1;
 
+        anim.SetFloat("Speed", Mathf.Abs(main1 * flipF));
+
+        if (main1 > 0 && !facingRight)
+        {
+            Flip();
+        }
+        else if (main1 < 0 && facingRight)
+        {
+            Flip();
+        }
     }
 
     void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(main1 * speed, rb.linearVelocity.y);
-
+        rb.linearVelocity = new Vector3(main1 * speed, rb.linearVelocity.y, rb.linearVelocity.z);
     }
 
-
-    void flip()
+    void Flip()
     {
-        if (!facingRight)
-        transform.localRotation = Quaternion.Euler(0, -90, 0);
-        else transform.localRotation = Quaternion.Euler(0, 90, 0);
         facingRight = !facingRight;
-
+        float yRotation = facingRight ? 90f : -90f;
+        transform.localRotation = Quaternion.Euler(0, yRotation, 0);
     }
 }
